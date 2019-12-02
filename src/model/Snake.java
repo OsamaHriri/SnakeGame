aca933676536f99a2eed6095023c003913d4ffa7
Odin <oh123602@gmail.com>
2019-12-02 11:24:43 +0200

    Added basic game engine

--- src/model/Snake.java
+++ src/model/Snake.java
@@ -1,91 +1,92 @@
 package model;
-
+import view.MainGameView;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Objects;
 
 public class Snake {
 
-    private int head;
-    private int tail;
-    private int length;
-    private int souls;
-    private int[][] placeOnBoard;
+    /**
+     * Snake's starting size
+     */
+    private static final int SIZE = 6;
+    private BodyPart head;
+    /**
+     * Array that holds the entire body
+     */
+    private ArrayList<BodyPart> body;
+    /**
+     * Snake's size variable and starting position of head
+     */
+    private int size, headX = MainGameView.WIDTH/2 + GameObject.SIZE/2, headY = MainGameView.HEIGHT/2 + GameObject.SIZE/2;
 
-    public Snake(int head, int tail, int length, int souls, int[][] placeOnBoard) {
-        this.head = head;
-        this.tail = tail;
-        this.length = length;
-        this.souls = souls;
-        this.placeOnBoard = placeOnBoard;
+    public Snake() {
+
+        body = new ArrayList<>();
+        head = new BodyPart(headX, headY);
+        size = 0;
+        setStart();
     }
 
-    public int getHead() {
-        return head;
+    /**
+     * Method to set snake at starting position, at start or restart of the game
+     */
+    public void setStart() {
+
+        // set starting position
+        if(size == 0) {
+
+            body.add(head);
+            ++size;
+
+            for(int i = 1; i < SIZE; ++i) {
+                addBodyPart(headX, headY + (i * GameObject.SIZE));
+            }
+        }
+        //if game restart
+        else {
+
+            body.clear();
+            head.setX(headX);
+            head.setY(headY);
+            size = 0;
+            setStart();
+        }
     }
 
-    public void setHead(int head) {
-        this.head = head;
+
+    /**
+     * Return snake's actual size
+     * @return size
+     */
+    public int getSize() {
+        return this.size;
     }
 
-    public int getTail() {
-        return tail;
+    /**
+     * Return particular body part
+     * @param i - position of part in body array
+     * @return the body part
+     */
+    public BodyPart getBodyPart(int i) {
+        return body.get(i);
     }
 
-    public void setTail(int tail) {
-        this.tail = tail;
+    /**
+     * Returns the head of the snake as BodyPart object
+     * @return snake's head
+     */
+    public BodyPart getHead() {
+        return this.head;
     }
 
-    public int getLength() {
-        return length;
-    }
-
-    public void setLength(int length) {
-        this.length = length;
-    }
-
-    public int getSouls() {
-        return souls;
-    }
-
-    public void setSouls(int souls) {
-        this.souls = souls;
-    }
-
-    public int[][] getPlaceOnBoard() {
-        return placeOnBoard;
-    }
-
-    public void setPlaceOnBoard(int[][] placeOnBoard) {
-        this.placeOnBoard = placeOnBoard;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Snake snake = (Snake) o;
-        return head == snake.head &&
-                tail == snake.tail &&
-                length == snake.length &&
-                souls == snake.souls &&
-                Arrays.equals(placeOnBoard, snake.placeOnBoard);
-    }
-
-    @Override
-    public int hashCode() {
-        int result = Objects.hash(head, tail, length, souls);
-        result = 31 * result + Arrays.hashCode(placeOnBoard);
-        return result;
-    }
-
-    @Override
-    public String toString() {
-        return "Snake{" +
-                "head=" + head +
-                ", tail=" + tail +
-                ", length=" + length +
-                ", souls=" + souls +
-                ", placeOnBoard=" + Arrays.toString(placeOnBoard) +
-                '}';
+    /**
+     * Adds new body part at given point
+     * @param x - position
+     * @param y - position
+     */
+    public void addBodyPart(int x, int y) {
+        body.add(new BodyPart(x,y));
+        ++size;
     }
 }
