42d31671a23e5c70ed185c3079d64abe7a0bfb27
fadi <frouhana@outlook.com>
2019-12-04 01:01:39 +0200

    Adding Classes

--- src/control/SysData.java
+++ src/control/SysData.java
@@ -5,8 +5,7 @@ import java.util.ArrayList;
 public class SysData {
 
     private static SysData single_instance = null;
-    private ArrayList<Game> history;
-    private ArrayList<Question> questions;
+
 
     public static SysData getInstance() {
         if (single_instance == null)
